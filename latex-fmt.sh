21ef2f9aa7dbcc61ab84ffeba72da6b67bc2677c
sam bacha <sam@manifoldfinance.com>
2022-06-01 03:05:28 -0700

    run against globbing

--- latex-fmt.sh
+++ latex-fmt.sh
@@ -1,3 +1,3 @@
-latexindent.pl -l=+mainfile.yaml -w -rv -m -s latexindent.tex
+latexindent.pl -l=+mainfile.yaml -w -rv -m -s *.tex
 latexindent.pl -w -rv -m -g tex-files.log -l -s s*.tex
 latexindent.pl -w -rv -m -g bib-files.log -l bibsettings.yaml -s *.bib
